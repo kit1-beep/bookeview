6f2cceab8ea0217bf14070ebec105ab1c4344b6f
kyle kitchener <kyle.kitchener@outlook.com>
2024-01-16 11:15:23 +0200

    first comming

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  define('DB_NAME', 'bookmvc');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/shareposts');
+  // Site Name
+  define('SITENAME', 'Book review');
+  // App Version
+  define('APPVERSION', '1.0.0');
---
+++ app/views/pages/index.php
@@ -0,0 +1,9 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <div class="jumbotron jumbotron-flud text-center">
+    <div class="container">
+      
+    <h1 class="display-3">Book reviews </h1>
+    <p class="lead"> </p>
+    </div>
+  </div> 
+<?php require APPROOT . '/views/inc/footer.php'; ?>
